9a83358119852fad5741cc818169fdf70f5d02cb
tenKre <tin_petrovic@yahoo.com>
2022-02-08 11:31:08 +0100

    fixed links

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar({inCart, itemsTotal}) {
     <div className="navbar-container">
      <h2>Webshop App</h2>
      <div className="navbar-links">
-         <h3><Link className="navbar-link" to="/webshop/git">Products </Link></h3>
+         <h3><Link className="navbar-link" to="/webshop/">Products </Link></h3>
          <h3><Link className="navbar-link basket" to="/webshop/basket">Basket 
          <span className="total">{`${itemsTotal > 0 ? itemsTotal : ""}\u20AC`}</span>
           <RiShoppingCart2Line/>
